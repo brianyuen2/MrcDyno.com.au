a6e4303c45853d14cc7868cbbf87c863de399bc3
Brian <brianyuen2@gmail.com>
2025-06-18 16:50:56 +1000

    update style

--- src/app/components/projects/index.tsx
+++ src/app/components/projects/index.tsx
@@ -25,7 +25,7 @@ const Card = (props: { projectName: string; filename: StaticImageData }) => {
   const { filename, projectName } = props;
   return (
     <div
-      className={`relative rounded-xl overflow-hidden transition-all duration-300 ease-in-out hover:scale-[1.02] hover:shadow-xl cursor-pointer group`}
+      className={`relative rounded-xl overflow-hidden transition-all duration-300 ease-in-out hover:scale-[1.01] hover:shadow-xl cursor-pointer group`}
     >
       <Image src={filename} alt={"Project image"} />
       <div className="absolute inset-0  group-hover:bg-white/15 transition-all duration-200"></div>
